e974a2cf0f3fa15f052dec1de376b05c0f29d7fa
gauravstr07 <gauravstr05@gmail.com>
2025-09-27 15:28:19 +0530

    PrintSum

---
+++ LC_04_PrintSum.py
@@ -0,0 +1,22 @@
+num1 = 50
+num2 = 45
+
+sum = num1 + num2
+
+#print(sum)
+
+
+def sum(a, b, c):
+    temp = 0
+    temp = a + b * c
+    return temp
+
+print(sum(20, 20, 2))
+
+
+def multiply(a, b):
+    return a * b
+
+print(multiply(num1, num2))
+
+
