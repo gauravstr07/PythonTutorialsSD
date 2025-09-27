1f5df379f22c7caf743de80eb80efed87396d2fd
gauravstr07 <gauravstr05@gmail.com>
2025-09-27 12:00:17 +0530

    DataTypes

---
+++ LC_02_DataTypes.py
@@ -0,0 +1,20 @@
+name = "Gaurav Sutar"
+age = 24
+email = "gauravsutar.tech86@gmail.com"
+empcode = "CE3375"
+old = False
+package = 65.75
+
+print(type(name))
+print(type(age))
+print(type(package))
+print(type(old))
+
+
+name1 = "Gaurav"
+name2 = 'Gaurav'
+name3 = '''Gaurav'''
+
+print(name1)
+print(name2)
+print(name3)
