00152cee3f59fc51e044c8e85adc3a8b979d6ce5
gauravstr07 <gauravstr05@gmail.com>
2025-09-28 14:33:24 +0530

    Conditions

---
+++ LC_01_StringAndConditions/LC_03_ConditionalStatements.py
@@ -0,0 +1,20 @@
+num1 = 5
+num2 = 10
+
+# if (num1 > num2) or (num2 > num1):
+#     print("Not")
+# else:
+#     print("Yes")
+
+age = 15
+canVote = False
+
+if(age >= 18):
+    canVote = True
+else:
+    canVote = False
+    
+if(canVote):
+    print("You Can Vote")
+else:
+    print("Get Out")
